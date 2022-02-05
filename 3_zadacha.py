03e96f99c2eff3c31d1d3ea24d7751575b7ca5f1
DenisAvocado <shelden2005@mail.ru>
2022-02-05 22:46:27 +0600

    3rd exercise

--- 3_zadacha.py
+++ 3_zadacha.py
@@ -2,6 +2,7 @@ import os
 import sys
 import requests
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic
 
@@ -13,14 +14,14 @@ class YandexMaps(QMainWindow):
         self.server = 'http://static-maps.yandex.ru/1.x/'
         self.request_params = {
             'll': '37.530887,55.703118',
-            'spn': '0.002,0.002',
+            'spn': '0.005,0.005',
             'l': 'map'
         }
         self.image_update()
 
     def image_update(self):
         response = requests.get(self.server, params=self.request_params)
-
+        print(response.url)
         if not response:
             print("Ошибка выполнения запроса:")
             print(self.map_request)
@@ -32,8 +33,35 @@ class YandexMaps(QMainWindow):
             file.write(response.content)
 
         self.pixmap = QPixmap(self.map_file)
-        self.pixmap = self.pixmap.scaled(650, 450)
         self.map.setPixmap(self.pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageDown:
+            self.scale_update('/')
+        elif event.key() == Qt.Key_PageUp:
+            self.scale_update('*')
+        else:
+            self.next_view(event)
+
+    def scale_update(self, action):
+        new_scale = eval(f"{float(self.request_params['spn'].split(',')[0])}{action}2")
+        if 0.0001562 < new_scale < 90:
+            self.request_params['spn'] = f'{new_scale},{new_scale}'
+            self.image_update()
+
+    def next_view(self, event):
+        l1, l2 = [float(l) for l in self.request_params['ll'].split(',')]
+        spn = float(self.request_params['spn'].split(',')[0])
+        if event.key() == Qt.Key_Up:
+            l2 += spn * 1.1
+        if event.key() == Qt.Key_Down:
+            l2 -= spn * 1.1
+        if event.key() == Qt.Key_Left:
+            l1 -= spn * 2.6
+        if event.key() == Qt.Key_Right:
+            l1 += spn * 2.6
+        self.request_params['ll'] = f'{str(l1)},{str(l2)}'
+        self.image_update()
 
     def closeEvent(self, event):
         os.remove(self.map_file)
