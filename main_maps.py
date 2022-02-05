d468e58d37365d8a4d7ebbecc33889a7bf406845
DenisAvocado <shelden2005@mail.ru>
2022-02-05 21:24:14 +0600

    takes from Andrey

---
+++ main_maps.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
+
+
+class YandexMaps(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('maps.ui', self)
+        self.server = 'http://static-maps.yandex.ru/1.x/'
+        self.request_params = {
+            'll': '37.530887,55.703118',
+            'spn': '0.002,0.002',
+            'l': 'map'
+        }
+        self.image_update()
+
+    def image_update(self):
+        response = requests.get(self.server, params=self.request_params)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(self.map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+        self.pixmap = QPixmap(self.map_file)
+        self.pixmap = self.pixmap.scaled(650, 450)
+        self.map.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        os.remove(self.map_file)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = YandexMaps()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
